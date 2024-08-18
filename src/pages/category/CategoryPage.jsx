a04699446f68357816352aea98c379cd3eb18f00
Chim Saina <chimsaina982gmail.com>
2024-08-18 19:31:00 +0700

    update

--- src/pages/category/CategoryPage.jsx
+++ src/pages/category/CategoryPage.jsx
@@ -5,7 +5,6 @@ import { Metadata } from "../../lib/Metadata";
 export const Categories = () => {
   return (
     <>
-      <div>
       <Metadata
           title="Category | TrovKa"
           description="Explore detailed service categories on TrovKa to find exactly what you need."
@@ -14,7 +13,6 @@ export const Categories = () => {
           thumbnail="https://i.ibb.co/s6D2gFC/trovka-icon.png"
         />
 
-      </div>
       <div className="container mx-auto px-4 sm:px-6 lg:px-8 -mt-12">
         <div className="my-6">
           <Line className="border-t-2 border-gray-300" />
